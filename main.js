69cddd12dcdcea614b311026008d0370ffa399a2
Zinal-Patel <zinalpatel0302@gmail.com>
2021-12-13 20:24:08 -0500

    Header styling completed

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const navMenu = document.getElementById("nav-menu-id");
-const navToggle = document.getElementById("nav-toggle-id");
-const navClose = document.getElementById("nav-close-id");
+// const navMenu = document.getElementById("nav-menu-id");
+// const navToggle = document.getElementById("nav-toggle-id");
+// const navClose = document.getElementById("nav-close-id");
+const navMenu = document.querySelector("#nav-menu-id");
+const navToggle = document.querySelector("#nav-toggle-id");
+const navClose = document.querySelector("#nav-close-id");
 
-/* MENU SHOW*/
-/* validate if const exists*/
-
+//SHOW Menu
 if(navToggle){
    
     navToggle.addEventListener('click',()=>{
@@ -12,11 +13,21 @@ if(navToggle){
     })    
 }
 
-/* MENU Hidden*/
-/* validate if const exists*/
-
-if(navClose){
-    navClose.addEventListener('click',()=>{
-        navMenu.classList.remove('show-menu');
-    })
+function linkAction(){
+    navMenu.classList.remove('show-menu');
 }
+
+//HIDE Menu when we click close
+if(navClose){
+   
+    navClose.addEventListener('click',linkAction());   
+}
+
+//HIDE Menu when we click on any of the navigation links/menu options
+
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+//array.forEach(element => console.log(element));
